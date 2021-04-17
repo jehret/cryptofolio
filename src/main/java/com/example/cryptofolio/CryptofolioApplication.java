d6f8b3fc44c0d8b77e0a1934321048f156410cd8
Jean-Philippe Ehret <jehret@mohio.earth>
2021-04-17 15:36:47 +1200

    comment

--- src/main/java/com/example/cryptofolio/CryptofolioApplication.java
+++ src/main/java/com/example/cryptofolio/CryptofolioApplication.java
@@ -2,6 +2,8 @@ package com.example.cryptofolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CryptofolioApplication {
@@ -10,11 +12,9 @@ public class CryptofolioApplication {
 		SpringApplication.run(CryptofolioApplication.class, args);
 	}
 
-	/* Pour la version serveur
-
 	@Bean
 	RestTemplate getRestTemplate(){
 		return new RestTemplate();
-	}*/
+	}
 
 }
